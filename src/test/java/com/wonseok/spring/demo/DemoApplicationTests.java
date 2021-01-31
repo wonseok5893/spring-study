273e7b0d2eb1b48f34510a0900a5f3de587bef0f
wonseok <wonseok5893@naver.com>
2021-02-01 03:03:25 +0900

    [INIT] initialize

---
+++ src/test/java/com/wonseok/spring/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wonseok.spring.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
