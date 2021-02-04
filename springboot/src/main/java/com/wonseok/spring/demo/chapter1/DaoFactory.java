0dc8928f3766cfbe5b82ee619f8f2021d662b2e6
wonseok <wonseok5893@naver.com>
2021-02-05 02:54:57 +0900

    반복되는 DConnectionMaker 주입을 리팩토링

--- springboot/src/main/java/com/wonseok/spring/demo/chapter1/DaoFactory.java
+++ springboot/src/main/java/com/wonseok/spring/demo/chapter1/DaoFactory.java
@@ -16,4 +16,9 @@ public class DaoFactory {
     public MessageDao messageDao(){
         return new MessageDao(new DConnectionMaker());
     }
+
+    //반복되는 코드를  처리하기 위해
+    public ConnectionMaker connectionMaker(){
+        return new DConnectionMaker();
+    }
 }
