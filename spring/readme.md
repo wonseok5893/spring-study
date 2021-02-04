6262a23f98f38c306d954223700cc04356f9c8d5
wonseok <wonseok5893@naver.com>
2021-02-04 21:56:28 +0900

    [CREATE] 1장 오브젝트와 의존관계 / 초난감 UserDao

--- spring/readme.md
+++
@@ -1,4 +0,0 @@
-# Intelli J Ultimate 필요 
-    
-    Intelli J Community : Spring 지원 x
-    License 필요
---
+++ springboot/src/main/java/com/wonseok/spring/demo/chapter1/UserDao.java
@@ -0,0 +1,38 @@
+package com.wonseok.spring.demo.chapter1;
+
+import javax.xml.transform.Result;
+import java.sql.*;
+
+public class UserDao {
+    public void add(User user)throws ClassNotFoundException, SQLException{
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection c = DriverManager.getConnection("jdbc:mysql://localhost/springbook","wonseok","1234");
+        PreparedStatement ps = c.prepareStatement("insert into users(id,name,password) values(?,?,?)");
+        ps.setString(1,user.getId());
+        ps.setString(2,user.getName());
+        ps.setString(3,user.getPassword());
+
+        ps.executeUpdate();
+
+        ps.close();
+        c.close();
+    }
+    public User get(String id)throws ClassNotFoundException, SQLException{
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection c = DriverManager.getConnection("jdbc:mysql://localhost/springbook","wonseok","1234");
+        PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
+        ps.setString(1,id);
+
+        ResultSet rs = ps.executeQuery();
+        rs.next();
+        User user = new User();
+        user.setId(rs.getString("id"));
+        user.setName(rs.getString("name"));
+        user.setPassword(rs.getString("password"));
+        rs.close();
+        ps.close();
+        c.close();
+        return user;
+    }
+
+}
