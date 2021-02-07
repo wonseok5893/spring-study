8b7c1534a33a2921403be000dee4c0503acca2b7
wonseok <wonseok5893@naver.com>
2021-02-07 18:03:26 +0900

    [FIX] build.gradle

--- springboot/src/main/java/com/wonseok/spring/demo/config/annotation/BeanConfig2.java
+++ springboot/src/main/java/com/wonseok/spring/demo/config/annotation/BeanConfig2.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class BeanConfig {
+public class BeanConfig2 {
     @Bean
     public MyBean myBean(){
         return new MyBeanImp();
--- springboot/src/main/java/com/wonseok/spring/demo/config/annotation/MyBeanTest.java
+++ springboot/src/main/java/com/wonseok/spring/demo/config/annotation/MyBeanTest.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class MyBeanTest {
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(BeanConfig.class);
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(BeanConfig2.class);
         MyBean myBean = context.getBean(MyBean.class);
         System.out.println(myBean.getClass());
         System.out.println(myBean.getName());
