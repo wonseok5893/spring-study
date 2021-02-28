fe2e210dd3ef69e09c41ea00eadeba2562b55ba1
wonseok <wonseok5893@naver.com>
2021-02-28 18:59:18 +0900

    XUnit 만들기 - @Annotation으로 대체하여 test메소드 실행

--- java-study/src/main/java/com/example/demo/xunit/TestCaseTest.java
+++ java-study/src/main/java/com/example/demo/xunit/TestCaseTest.java
@@ -1,5 +1,7 @@
 package com.example.demo.xunit;
 
+import com.example.demo.xunit.annotation.Test;
+
 public class TestCaseTest extends TestCase {
 
     public TestCaseTest(String testMethod) {
@@ -10,34 +12,35 @@ public class TestCaseTest extends TestCase {
         return new TestSuite(TestCaseTest.class);
     }
 
+    @Test
     public void testTemplateMethod() {
         WasRun wasRun = new WasRun("testMethod");
         TestResult result = new TestResult();
         wasRun.run(result);
         Assert.assertEquals("setUp testMethod tearDown", wasRun.log);
     }
-
-    public void testResult() {
+    @Test
+    public void Result() {
         WasRun wasRun = new WasRun("testMethod");
         TestResult result = new TestResult();
         wasRun.run(result);
         Assert.assertEquals("1 run, 0 failed", result.getSummary());
     }
-
+    @Test
     public void testFailedResultFormatting() {
         TestResult result = new TestResult();
         result.testStarted();
         result.testFailed();
         Assert.assertEquals("1 run, 1 failed",result.getSummary());
     }
-
+    @Test
     public void testFailedResult() {
         WasRun wasRun = new WasRun("testBrokenMethod");
         TestResult result = new TestResult();
         wasRun.run(result);
         Assert.assertEquals("1 run, 1 failed", result.getSummary());
     }
-
+    @Test
     public void testSuite() {
         TestSuite suite = new TestSuite();
         suite.add(new WasRun("testMethod"));
--- java-study/src/main/java/com/example/demo/xunit/TestSuite.java
+++ java-study/src/main/java/com/example/demo/xunit/TestSuite.java
@@ -1,5 +1,7 @@
 package com.example.demo.xunit;
 
+import org.springframework.core.annotation.AnnotationUtils;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -11,9 +13,23 @@ public class TestSuite implements Test {
     public TestSuite() {
     }
 
+    //refection method 이름 앞에 test / 생성자 파라미터로 String을 하나 받는 클래스 객체를 생성하여 add 해준다
+//    public TestSuite(Class<? extends Test> testClass) {
+//        Arrays.stream(testClass.getDeclaredMethods())
+//                .filter(m -> m.getName().startsWith("test"))
+//                .forEach(m -> {
+//                            try {
+//                                add(testClass.getConstructor(String.class).newInstance(m.getName()));
+//                            } catch (Exception e) {
+//                                throw new RuntimeException(e);
+//                            }
+//                        }
+//                );
+//    }
+
     public TestSuite(Class<? extends Test> testClass) {
         Arrays.stream(testClass.getDeclaredMethods())
-                .filter(m -> m.getName().startsWith("test"))
+                .filter(m -> AnnotationUtils.findAnnotation(m, com.example.demo.xunit.annotation.Test.class)!=null)
                 .forEach(m -> {
                             try {
                                 add(testClass.getConstructor(String.class).newInstance(m.getName()));
