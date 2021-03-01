f18dae431ce2e0b5fbdf17e010a12be3ca774204
wonseok <wonseok5893@naver.com>
2021-03-01 20:47:58 +0900

    TobySpringBoot1 - ApplicationRunner, CommandLineRunner 차이

--- tobyspringboot1/src/main/java/com/example/tobyspringboot1/Tobyspringboot1Application.java
+++ tobyspringboot1/src/main/java/com/example/tobyspringboot1/Tobyspringboot1Application.java
@@ -1,5 +1,6 @@
 package com.example.tobyspringboot1;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,15 +13,15 @@ public class Tobyspringboot1Application {
         SpringApplication.run(Tobyspringboot1Application.class, args);
     }
     /*
-    CommandLineRunner
-    ApplicationRunner 1.3
+    CommandLineRunner, -> args를 받을 때 실행됨
+    ApplicationRunner, 1.3 에 도입-> SpringApplication.run()이 실행될때 실행됨
     의 차이
      */
 
     //인라인으로 빈을 정의하는 방법
     //이제는 @Configuration이 붙지 않아도 @Bean 등록 가능 / 하지만 동작방식이 다르다
     @Bean
-    public CommandLineRunner myClRunner() {
+    public ApplicationRunner myClRunner() {
         return args -> System.out.println("Hello AnnonyMous CommandLineRunner!!!");
     }
 }
