58e933f307b3da021191b9f674b76df0bdbfd53f
wonseok <wonseok5893@naver.com>
2021-03-01 20:52:30 +0900

    TobySpringBoot1 - EventListener를 이용한 초기화 방법

--- tobyspringboot1/src/main/java/com/example/tobyspringboot1/Tobyspringboot1Application.java
+++ tobyspringboot1/src/main/java/com/example/tobyspringboot1/Tobyspringboot1Application.java
@@ -4,7 +4,9 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 @SpringBootApplication
@@ -12,16 +14,9 @@ public class Tobyspringboot1Application {
     public static void main(String[] args) {
         SpringApplication.run(Tobyspringboot1Application.class, args);
     }
-    /*
-    CommandLineRunner, -> args를 받을 때 실행됨
-    ApplicationRunner, 1.3 에 도입-> SpringApplication.run()이 실행될때 실행됨
-    의 차이
-     */
 
-    //인라인으로 빈을 정의하는 방법
-    //이제는 @Configuration이 붙지 않아도 @Bean 등록 가능 / 하지만 동작방식이 다르다
-    @Bean
-    public ApplicationRunner myClRunner() {
-        return args -> System.out.println("Hello AnnonyMous CommandLineRunner!!!");
+    @EventListener(ApplicationReadyEvent.class)
+    public void init() {
+        System.out.println("Hello ApplicationReadyEvent");
     }
 }
