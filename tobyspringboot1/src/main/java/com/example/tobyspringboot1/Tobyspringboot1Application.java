c7e9056b417347c7bd19238dc36e31647b31749c
wonseok <wonseok5893@naver.com>
2021-03-01 20:40:45 +0900

    TobySpringBoot1 - inline으로 빈등록 / @Configuration이 없어도 어디서든 @Bean으로 빈등록이가능 / 하지만 동작방식이 다름

--- tobyspringboot1/src/main/java/com/example/tobyspringboot1/Tobyspringboot1Application.java
+++ tobyspringboot1/src/main/java/com/example/tobyspringboot1/Tobyspringboot1Application.java
@@ -3,6 +3,7 @@ package com.example.tobyspringboot1;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @SpringBootApplication
@@ -15,8 +16,15 @@ public class Tobyspringboot1Application {
     ApplicationRunner 1.3
     의 차이
      */
+
+    //인라인으로 빈을 정의하는 방법
+    //이제는 @Configuration이 붙지 않아도 @Bean 등록 가능 / 하지만 동작방식이 다르다
+    @Bean
+    public MyClRunner myClRunner() {
+        return new MyClRunner();
+    }
 }
-@Component
+
 class MyClRunner implements CommandLineRunner{
 
     @Override
