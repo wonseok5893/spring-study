602c86fae6393aae0fa4cc77d2d0a8adb0f91c7a
wonseok <wonseok5893@naver.com>
2021-03-01 21:12:52 +0900

    TobySpringBoot1 - EventListener (2)

--- tobyspringboot1/src/main/java/com/example/tobyspringboot1/Tobyspringboot1Application.java
+++ tobyspringboot1/src/main/java/com/example/tobyspringboot1/Tobyspringboot1Application.java
@@ -10,6 +10,7 @@ import org.springframework.context.ApplicationListener;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.event.EventListener;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @SpringBootApplication
@@ -26,6 +27,11 @@ public class Tobyspringboot1Application {
         System.out.println("Hello My Event");
     }
 
+    @EventListener(MyEvent.class)
+    public void onMyEvent2() {
+        System.out.println("Hello My Event2");
+    }
+    // Event는 -> MutiCast 하게 모든 listener에게 날라간다. 순서 보장x-> @Order 로 처리
     static class MyEvent extends ApplicationEvent {
 
         private final String message;
