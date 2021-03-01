057cbd59111b79526b12cd6e18d5fac0cc8eafbd
wonseok <wonseok5893@naver.com>
2021-03-01 20:37:21 +0900

    TobySpringBoot1 - CommandLineRunner

--- tobyspringboot1/src/main/java/com/example/tobyspringboot1/Tobyspringboot1Application.java
+++ tobyspringboot1/src/main/java/com/example/tobyspringboot1/Tobyspringboot1Application.java
@@ -1,15 +1,26 @@
 package com.example.tobyspringboot1;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class Tobyspringboot1Application {
-    public static void main(String[] args)
-    {
-        System.out.println("Hello1");
+    public static void main(String[] args) {
         SpringApplication.run(Tobyspringboot1Application.class, args);
-        System.out.println("Hello2");
     }
+    /*
+    CommandLineRunner
+    ApplicationRunner 1.3
+    의 차이
+     */
+}
+@Component
+class MyClRunner implements CommandLineRunner{
 
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Hello CommandLineRunner");
+    }
 }
